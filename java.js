a36cde8e8616032401a1ad1ddc50adf98cbdc529
khulood05 <khuloodazam06@gmail.com>
2025-08-02 13:40:20 +0530

    Add files via upload

---
+++ java.js
@@ -0,0 +1,23 @@
+const button = document.getElementById("btn");
+const colorText = document.querySelector(".color");
+
+const hex = ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f']
+
+button.addEventListener("click" ,()=>{
+    const hexColor = generatehexcolor();
+    document.body.style.backgroundColor = hexColor;
+    colorText.textContent = hexColor;
+}
+);
+
+function generatehexcolor(){
+    let hexColor = "#"
+    for(i=0; i<6; i++){
+        hexColor = hexColor + hex[generateRandomNumber()]
+    }
+    return hexColor;
+};
+
+function generateRandomNumber(){
+    return Math.floor(Math.random() * hex.length)
+}
